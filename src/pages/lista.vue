76ea8e5ffd2cdb7f5c292f271f44e790bd4402c4
Helton Carlos <heltoncarlospa@hotmail.com>
2021-11-24 23:07:47 -0300

    testando tags

---
+++ src/pages/lista.vue
@@ -0,0 +1,45 @@
+<template>
+    <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Meus contatos</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    
+    <ion-content >
+      <ion-list>
+        <ion-toolbar>
+          <ion-item v-for="list in arrayLista" :key="list">
+           <ion-label>{{list}}</ion-label>
+          </ion-item>
+        </ion-toolbar>
+      </ion-list>
+    
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,IonItem,IonLabel,IonList } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,IonItem,IonLabel,IonList
+  },
+  setup() {
+      return{
+          arrayLista:["Helton","Elisa","Pardal"]
+      }
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
